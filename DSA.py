c38ff620d015ddd69f23818b72af7aa86721f08b
shrashti mishra <shrashtimishra722@gmail.com>
2026-08-24 14:46:12 +0530

    main

---
+++ DSA.py
@@ -0,0 +1,35 @@
+# numbers=[10,20,30,40]
+# total=0
+# for num in numbers:
+#     total=total+num
+      
+# print(total)
+
+
+# numbers=[12,7,24,7,15,5,42]
+# count=0
+# for num in numbers:
+#    if num%2==0:
+#     count=count+1
+# print(count)
+
+# numbers=[12,7,24,7,15,5,42]
+# count=0
+# for num in numbers:
+#   if num%2!=0:
+#     count=count+1
+# print(count)
+
+
+# numbers=[12,7,24,7,15,5,42]
+# target=24
+# for num in numbers:
+#   if num==target:
+#     print("found")
+
+# numbers=[2,3,2,4,2,5,2]
+# count=0
+# for num in numbers:
+#   if num==2:
+#     count=count+1
+# print(count)
